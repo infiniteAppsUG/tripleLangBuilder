6ad4027356fa64612c1aa55461b3ffdb7e67ca23
infiniteApps <p-a.reiners@infiniteapps.de>
2023-12-10 19:18:20 +0100

    removed unnecessary stuff

--- bin/l10n_translator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:l10n_translator/l10n_translator.dart' as l10n_translator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${l10n_translator.calculate()}!');
-}
--- test/l10n_translator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:l10n_translator/l10n_translator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
